aca4b7c47977e87ee449ba4f6ca8ef10a6c3ec86
rashmi <grashmishankar@gmail.com>
2020-08-31 22:33:51 -0700

    creating custom login page

--- Freshvotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
+++ Freshvotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -17,9 +17,9 @@ public class WebSecurityConfiguration  extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
 		http.csrf().disable()
-		.authorizeRequests().antMatchers("/").permitAll()
+		.authorizeRequests().antMatchers("/").permitAll().antMatchers("/login").permitAll()
 		.anyRequest().hasRole("USER")
-		.and().formLogin().loginPage("/login")
+		.and().formLogin().loginPage("/login").permitAll()
 		.and().logout().logoutUrl("/logout").permitAll();
 	}
 
